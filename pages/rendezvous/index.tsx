e6e774d23a3348e5e3c9506bc31e6d90ed0965dd
Thales Minoru Sato <thalesms2@outlook.com>
2022-07-08 13:45:42 -0300

    Create index.tsx

---
+++ pages/rendezvous/index.tsx
@@ -0,0 +1,46 @@
+import type { NextPage } from 'next'
+import { useState } from 'react'
+import styled, { keyframes } from 'styled-components'
+
+const teste = keyframes`
+  from {
+    transform: translateX(0px);
+  }
+  to {
+    transform: translateX(-150px);
+    background-color: red
+  }
+`
+
+const Wrapper = styled.div`
+  width: 300px;
+  height: 300px;
+  background-color: black;
+`
+const WrapperActive = styled.div`
+  width: 300px;
+  height: 300px;
+  background-color: black;
+  animation: ${teste} linear 2s;
+`
+
+const Button = styled.button`
+  color: white;
+  background-color: black;
+  padding: .5em 1em;
+  margin: .5em;
+`
+
+const Home: NextPage = () => {
+  const [anim, setAnim] = useState(true)
+  return (
+    <>
+      { anim ? 
+        <Wrapper/> :
+        <WrapperActive /> }
+      <Button onClick={() => setAnim(!anim)}>Change</Button>
+    </>
+  )
+}
+
+export default Home
