e439ced9963c1550a6440647f115f7e074fc5359
Thales Minoru Sato <thalesms2@outlook.com>
2022-07-11 17:34:08 -0300

    feat: border-radius editor

---
+++ pages/shorts/border-radius.tsx
@@ -0,0 +1,87 @@
+import { NextPage } from 'next'
+import React from 'react'
+import styled from 'styled-components'
+import Slider from '@mui/material/Slider';
+
+
+interface ExampleProps {
+    topLeft: string
+    topRight: string
+    bottomRight: string
+    bottomLeft: string
+}
+
+const Wrapper = styled.div`
+    background-color: #000;
+`
+
+const Example = styled.div<ExampleProps>`
+    background-color: #808080;
+    width: 300px;
+    height: 300px;
+    border-radius: ${props => props.topLeft} ${props => props.topRight} ${props => props.bottomRight} ${props => props.bottomLeft};
+`
+
+const BorderRadius: NextPage = () => {
+    const [topLeft, setTopLeft] = React.useState(0)
+    const [topRight, setTopRight] = React.useState(0)
+    const [bottomRight, setBottomRight] = React.useState(0)
+    const [bottomLeft, setBottomLeft] = React.useState(0)
+    
+    function valuetext(value: number) {
+        return `${value}%`;
+    }
+
+    return (
+        <Wrapper>
+            <Example 
+                topLeft={`${topLeft}%`}
+                topRight={`${topRight}%`}
+                bottomRight={`${bottomRight}%`}
+                bottomLeft={`${bottomLeft}%`}
+            />
+            <Slider
+                aria-label="Porcentagem"
+                value={topLeft}
+                onChange={(e: any) => setTopLeft(e.target.value)}
+                getAriaValueText={valuetext}
+                valueLabelDisplay="auto"
+                step={1}
+                min={0}
+                max={100}
+            />
+            <Slider
+                aria-label="Porcentagem"
+                value={topRight}
+                onChange={(e: any) => setTopRight(e.target.value)}
+                getAriaValueText={valuetext}
+                valueLabelDisplay="auto"
+                step={1}
+                min={0}
+                max={100}
+            />
+            <Slider
+                aria-label="Porcentagem"
+                value={bottomRight}
+                onChange={(e: any) => setBottomRight(e.target.value)}
+                getAriaValueText={valuetext}
+                valueLabelDisplay="auto"
+                step={1}
+                min={0}
+                max={100}
+            />
+            <Slider
+                aria-label="Porcentagem"
+                value={bottomLeft}
+                onChange={(e: any) => setBottomLeft(e.target.value)}
+                getAriaValueText={valuetext}
+                valueLabelDisplay="auto"
+                step={1}
+                min={0}
+                max={100}
+            />
+        </Wrapper>
+    )
+}
+
+export default BorderRadius
